0df902b8f31a7c6dce7ebfee720ffcd1cd08b68b
Sirius Sam <simsam@gmail.com>
2014-09-15 17:34:13 +0200

    Scrapyard : a Shared Inventory. Initial commit.
    Scrapyard is a plugin for KSP that adds a persisyent inventory for parts
    and resources.

---
+++ Scrapyard/EditPartListPrefab.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Scrapyard
+{
+    /// <summary>
+    ///  Courtesy of xEvilReaperx, edited by Sirius Sam
+    /// </summary>
+    [KSPAddon(KSPAddon.Startup.EditorAny, false)]
+    class EditPartListPrefab : MonoBehaviour
+    {
+        enum PartType { Part, Resource, Folder}
+
+        class ClickListener : MonoBehaviour
+        {
+            AvailablePart myPart;
+            EditorPartIcon icon;
+            SpriteText qty, title;
+            bool experimental;
+            PartType type;
+
+            public int QtyOnAssembly
+            {
+                get {
+                    ShipConstruct construct = EditorLogic.fetch.ship;
+                    if(construct != null) {
+                        if(type == PartType.Resource) {
+                            double total = 0;
+                            foreach(Part P in construct.Parts) foreach (PartResource R in P.Resources) if(R.resourceName == myPart.name) total += R.amount;
+                            return (int)total;
+                        } else return construct.Parts.FindAll(p => p.partInfo.name == myPart.name).Count;
+                    } else return 0;
+                }
+            }
+            public int QtyAvailable { get { return Scrapyard.Instance != null ? (int)(type == PartType.Resource ? Scrapyard.Instance.Resources: Scrapyard.Instance.Parts).Get(myPart.name) : 0; } }
+
+            void Start()
+            {
+
+                myPart = GetComponent<EditorPartIcon>().partInfo;
+                icon = GetComponent<EditorPartIcon>();
+                qty = transform.Find("CounterLabel").GetComponent<SpriteText>();
+                experimental = Scrapyard.isExperimental(myPart);
+                type = myPart.name == "Resources" ? PartType.Folder : Scrapyard.isResource(myPart.name) ? PartType.Resource : PartType.Part;
+
+                if (type == PartType.Folder)
+                {
+                    icon.SetGrey("Resources cannot be assembled.");
+                    qty.transform.Translate(new Vector3(-0.8f * EditorPartList.Instance.iconSize, EditorPartList.Instance.iconSize * 0.85f, 0), Space.Self);
+                    qty.SetAnchor(SpriteText.Anchor_Pos.Upper_Left);
+                    qty.alignment = SpriteText.Alignment_Type.Left;
+                    GetComponent<UIButton>().AddValueChangedDelegate(TankClicked);
+                }
+                else if (type == PartType.Resource)
+                {
+                    icon.SetGrey("Resources cannot be assembled.");
+                    title = transform.Find("TitleLabel").GetComponent<SpriteText>();
+                    title.Text = myPart.name.Length>=10? myPart.name.Substring(0,10):myPart.name;
+                    GetComponent<UIButton>().AddValueChangedDelegate(ResourceClicked);
+                }
+                UpdateCounter();
+            }
+
+            void TankClicked(IUIObject obj)
+            {
+                Scrapyard.resourceDisplay = Resource_Display.Detail;
+                EditorPartList.Instance.Refresh();
+            }
+            void ResourceClicked(IUIObject obj)
+            {
+                Scrapyard.resourceDisplay = Resource_Display.Summary;
+                EditorPartList.Instance.Refresh();
+            }
+
+            public void UpdateCounter()
+            {
+                if (type == PartType.Folder)
+                {
+                    int i = 0;
+                    foreach (string s in Scrapyard.Instance.Resources.List.Keys)
+                    {
+                        if (i++ > 4) continue;
+                        float a = 0;
+                        foreach (Part P in EditorLogic.fetch.ship.Parts) foreach (PartResource R in P.Resources) if (R.resourceName == s) a += (float)R.amount;
+                        float b = Scrapyard.Instance.Resources.Get(s), total = b - a;
+                        qty.Text += String.Format(total < 0 ? Scrapyard.TankMissingFormat : Scrapyard.TankFormat,
+                            a, b, total, (int)(-total * Scrapyard.getResourcePrice(s)), Scrapyard.getResourceCode(s));
+                    }
+                }
+                else
+                {
+                    int a = QtyOnAssembly, b = QtyAvailable, total = b - a;
+                    if (experimental && total <= 0) icon.SetGrey("Ran out of this item");
+                    if (total < 0)
+                    {
+                        qty.Color = Color.red;
+                        qty.Text = String.Format(Scrapyard.MissingQtyFormat, a, b, total, (int)(-total * (type == PartType.Resource ? Scrapyard.getResourcePrice(myPart.name) : Scrapyard.getPartRawPrice(myPart))) );
+                    }
+                    else
+                    {
+                        qty.Color = Color.white;
+                        qty.Text = String.Format(Scrapyard.QtyFormat, a,b,total);
+                    }
+                }
+            }
+        }
+        
+
+        public static EditPartListPrefab Instance { private set; get; }
+        void Start()
+        {
+            // VAB events
+            GameEvents.onPartAttach.Add(RefreshList);
+            GameEvents.onPartRemove.Add(RefreshList);
+
+
+            var iconPrefab = EditorPartList.Instance.iconPrefab.gameObject;
+            if (iconPrefab.GetComponent<ClickListener>() == null)
+            {
+                // Quantity
+                GameObject labelHolder = new GameObject("CounterLabel");
+                var label = labelHolder.AddComponent<SpriteText>();
+                label.RenderCamera = Camera.allCameras.Where(c => (c.cullingMask & (1 << labelHolder.layer)) != 0).Single();
+                labelHolder.layer = LayerMask.NameToLayer("EzGUI_UI");
+                labelHolder.transform.parent = iconPrefab.transform;
+                labelHolder.transform.localPosition = Vector3.zero;
+                labelHolder.transform.Translate(new Vector3(EditorPartList.Instance.iconSize * 0.35f, EditorPartList.Instance.iconSize * -0.425f, label.RenderCamera.nearClipPlane - labelHolder.transform.position.z - 1f), Space.Self);
+                label.Text = String.Empty;
+                label.alignment = SpriteText.Alignment_Type.Right;
+                label.font = UIManager.instance.defaultFont;
+                label.renderer.sharedMaterial = UIManager.instance.defaultFontMaterial;
+                label.SetColor(Color.white);
+                label.SetAnchor(SpriteText.Anchor_Pos.Lower_Right);
+                label.SetCharacterSize(12f);
+                DontDestroyOnLoad(labelHolder);
+
+                // Title of resources
+                GameObject titleHolder = new GameObject("TitleLabel");
+                var title = titleHolder.AddComponent<SpriteText>();
+                title.RenderCamera = Camera.allCameras.Where(c => (c.cullingMask & (1 << titleHolder.layer)) != 0).Single();
+                titleHolder.layer = LayerMask.NameToLayer("EzGUI_UI");
+                titleHolder.transform.parent = iconPrefab.transform;
+                titleHolder.transform.localPosition = Vector3.zero;
+                titleHolder.transform.Translate(new Vector3(-EditorPartList.Instance.iconSize * 0.425f, EditorPartList.Instance.iconSize * 0.425f, title.RenderCamera.nearClipPlane - titleHolder.transform.position.z - 1f), Space.Self);
+                title.Text = String.Empty;
+                title.alignment = SpriteText.Alignment_Type.Left;
+                title.font = UIManager.instance.defaultFont;
+                title.renderer.sharedMaterial = UIManager.instance.defaultFontMaterial;
+                title.SetColor(Color.white);
+                title.SetAnchor(SpriteText.Anchor_Pos.Upper_Left);
+                title.SetCharacterSize(12f);
+                DontDestroyOnLoad(titleHolder);
+
+                iconPrefab.AddComponent<ClickListener>();
+            }
+            EditorPartList.Instance.ExcludeFilters.AddFilter(new EditorPartListFilter("Scrapyard", FilterPart));
+            EditorPartList.Instance.Refresh();
+        }
+
+        void OnDestroy()
+        {
+            GameEvents.onPartAttach.Remove(RefreshList);
+            GameEvents.onPartRemove.Remove(RefreshList);
+        }
+
+        /// <summary>
+        /// Parts will be displayed if:
+        /// 1. they are accessible in the research tree
+        /// 2. they are not accessible but some quantity is available in the inventory.
+        /// </summary>
+        /// <param name="part">Part to test</param>
+        /// <returns>true if the part should be displayed in the list</returns>
+        public bool FilterPart(AvailablePart part)
+        {
+            if (part.name == "Resources")
+            {
+                return Scrapyard.Instance.recoverResources && Scrapyard.resourceDisplay == Resource_Display.Summary;
+            }
+            else if (Scrapyard.hasResourceCode(part.name))
+            {
+                return Scrapyard.Instance.recoverResources && Scrapyard.resourceDisplay == Resource_Display.Detail && Scrapyard.Instance.Resources.Get(part.name) != 0;
+            }
+            else if (Scrapyard.isExperimental(part))
+            {
+                int qty = (int)Scrapyard.Instance.Parts.Get(part.name);
+                bool available = qty > 0;
+                if (available && !ResearchAndDevelopment.IsExperimentalPart(part)) ResearchAndDevelopment.AddExperimentalPart(part);
+                return available;
+            }
+            else return true;
+        }
+
+        void Update()
+        {
+            if (EditorLogic.fetch != null && EditorLogic.fetch.ship != null)
+            {
+                Part selected = EditorLogic.SelectedPart;
+                // If a part is selected, and it's not the StartPod
+                if (selected != null && selected != EditorLogic.startPod)
+                {
+                    // is it possible to place it?
+                    ShipConstruct construct = EditorLogic.fetch.ship;
+                    Dictionary<AvailablePart, int> selectedParts = Scrapyard.ListAllParts(selected);
+
+                    foreach (AvailablePart id in selectedParts.Keys)
+                    {
+                        // this part is experimental
+                        if (ResearchAndDevelopment.GetTechnologyState(id.TechRequired) != RDTech.State.Available)
+                        {
+                            int a = (int)Scrapyard.Instance.Parts.Get(id.name), b = construct.Parts.FindAll(x => x.partInfo.name == id.name).Count, c = selectedParts[id];
+                            int available = a - b - c;
+                            if (available < 0)
+                            {
+                                EditorLogic.fetch.DestroySelectedPart();
+                                selected = null;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        void RefreshList(GameEvents.HostTargetAction<Part, Part> action)
+        {
+            if (EditorPartList.Instance != null) EditorPartList.Instance.Refresh();
+        }
+
+    }
+}
---
+++ Scrapyard/Scrapyard.cs
@@ -0,0 +1,356 @@
+﻿/*
+    KSP4X, written by Sami Boustani, aka Sirius Sam.
+*/
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using KSP.IO;
+
+namespace Scrapyard
+{
+    public enum Resource_Display { Summary, Detail }
+
+    public class Catalog
+    {
+        public Dictionary<string, float> List = new Dictionary<string, float>();
+
+        public void Add(string key, float qty = 1)
+        {
+            float total = 0;
+            List[key] = List.TryGetValue(key, out total) ? total + qty : qty;
+        }
+        public void Remove(string key, float qty = 1)
+        {
+            Add(key, -qty);
+        }
+        public float Get(string key)
+        {
+            if (!String.IsNullOrEmpty(key))
+            {
+                float qty;
+                return List.TryGetValue(key, out qty) ? qty : 0;
+            }
+            else return 0;
+        }
+        public void Set(string key, float qty)
+        {
+            List[key] = qty;
+        }
+    }
+
+    public class Scrapyard
+    {
+        static Scrapyard instance = null;
+        static Dictionary<string, string> resourceCodes = new Dictionary<string, string> {
+                {"LiquidFuel","LF"},
+                {"Oxidizer","Ox"},
+                {"SolidFuel","SF"},
+                {"MonoPropellant","MP"},
+                {"XenonGas","Xe"}
+        };
+
+        public Catalog Parts = new Catalog();
+        public Catalog Resources = new Catalog();
+        public static Scrapyard Instance { get { if (instance == null) instance = new Scrapyard(); return instance; } }
+
+        #region Settings
+        public static string QtyFormat = "{2}", //{0} = Quantity on assembly line, {1} = qty in stock, {2} = available - instore, 
+                        MissingQtyFormat = "({3}$) {2}", //{3} = cost of missing resource
+                        TankMissingFormat = "{4} {2}\n", // {4} = resource code
+                        TankFormat = "{4} {2}\n";
+        public Boolean recoverResources = true;
+        public static Resource_Display resourceDisplay = Resource_Display.Detail;
+        #endregion
+
+        #region I/O
+        /// <summary>
+        /// Called when the game is saved, commits inventory values into save game.
+        /// </summary>
+        /// <param name="root">ConfigNode passed from GameEvent</param>
+        public void Commit(ConfigNode root)
+        {
+            // Parts
+            ConfigNode N = new ConfigNode(this.GetType().FullName);
+            ConfigNode p = N.AddNode("PARTS");
+            foreach (string P in Parts.List.Keys.Where(x => Parts.List[x] != 0)) p.AddValue(P, Parts.List[P]);
+            // Resources, load even if recover_resources = false
+            ConfigNode r = N.AddNode("RESOURCES");
+            foreach (string P in Resources.List.Keys.Where(x => Resources.Get(x) != 0)) r.AddValue(P, Resources.List[P]);
+
+            root.AddNode(N);
+        }
+        /// <summary>
+        /// Called when the game is loaded, loads inventory items into Parts list.
+        /// </summary>
+        /// <param name="root"></param>
+        public void Rollback(ConfigNode root)
+        {
+            ConfigNode node = root.GetNode(this.GetType().FullName);
+            if (node == null) throw new Exception("Inventory.Rollback() : instance not found, cancelling");
+            else
+            {
+                // Load parts
+                ConfigNode pnode = node.GetNode("PARTS");
+                Parts.List.Clear();
+                int qty = 0;
+                foreach (ConfigNode.Value P in pnode.values)
+                {
+                    if (int.TryParse(P.value, out qty) && qty != 0) Parts.Set(P.name, qty);
+                }
+
+                // Load resources, even if recover_resources = false
+                ConfigNode rnode = node.GetNode("RESOURCES");
+                Resources.List.Clear();
+                float Qty = 0;
+                foreach (ConfigNode.Value P in rnode.values)
+                {
+                    if (float.TryParse(P.value, out Qty) && Qty != 0) Resources.Set(P.name, Qty);
+                }
+            }
+
+            
+        }
+        #endregion
+
+
+        #region Utility functions
+        /// <summary>
+        /// Stores a construct into the inventory, recursing on all subparts
+        /// </summary>
+        /// <param name="part">The root part from which recursion starts</param>
+        public void Store(Part part)
+        {
+            Parts.Add(part.partInfo.name);
+            foreach (Part P in part.children) Store(P);
+        }
+        /// <summary>
+        /// Retrieves a construct from the inventory, recursing on all subparts
+        /// </summary>
+        /// <param name="part">The root part from which recursion starts</param>
+        public void Retrieve(Part part)
+        {
+            Parts.Remove(part.partInfo.name);
+            foreach (Part P in part.children) Retrieve(P);
+        }
+
+        /// <summary>
+        /// Ship is rolled out, remove all parts present in the inventory and refund them
+        /// </summary>
+        /// <param name="construct"></param>
+        public void  RolloutVessel(ShipConstruct construct) {
+            String txt = "The following parts were in your inventory and were refunded :\n";
+            float totalRefund = 0;           
+
+            // Refund raw part price
+            Dictionary<AvailablePart, int> parts = ListAllParts(construct.Parts);
+            foreach (AvailablePart P in parts.Keys)
+            {
+                float rawprice = getPartRawPrice(P),
+                    qty_on_ship = parts[P],
+                    qty_in_store = (int)Parts.Get(P.name),
+                    qty_used = Math.Min(qty_on_ship, qty_in_store);
+
+                Parts.Set(P.name, Math.Max(0, qty_in_store - qty_on_ship));
+                if (qty_used > 0)
+                {
+                    float Refund = qty_used * rawprice;
+                    txt += String.Format("{0} * {1} = {2}$\n", (int)qty_used, P.title, (int)Refund);
+                    totalRefund += Refund;
+                }
+            }
+
+            // Refund resources if activated
+            if (recoverResources)
+            {
+                Dictionary<string, double> resources = ListAllResources(construct.Parts);
+                foreach (string R in resources.Keys)
+                {
+                    float price = getResourcePrice(R);
+                    if (price != 0)
+                    {
+                        float qty_on_ship = (float)resources[R],
+                            qty_in_store = Resources.Get(R),
+                            qty_used = Math.Min(qty_on_ship, qty_in_store),
+                            unitCost = getResourcePrice(R);
+
+                        if (qty_used > 0)
+                        {
+                            txt += String.Format("{1} * {0} * {2}$ = {3}$\n", R, qty_on_ship, unitCost, qty_on_ship * unitCost);
+                            Resources.Set(R, Math.Max(0, qty_in_store - qty_on_ship));
+                            totalRefund += (float)qty_used * getResourcePrice(R);
+                        }
+                    }
+                }
+            }
+            Funding.Instance.Funds += totalRefund;
+            if (totalRefund > 0) PopupDialog.SpawnPopupDialog("You were refunded " + totalRefund + "$", txt, "ok!", false, HighLogic.Skin);
+        }
+        /// <summary>
+        /// Ship has been recovered, store all parts and resources and cancel the stock refund.
+        /// </summary>
+        /// <param name="vessel"></param>
+        public void RecoverVessel(ProtoVessel vessel)
+        {
+            foreach (ProtoPartSnapshot P in vessel.protoPartSnapshots)
+            {
+                // recover part
+                Parts.Add(P.partInfo.name);
+                Funding.Instance.Funds -= getPartRawPrice(P.partInfo);
+
+                // recover part's resources
+                if (recoverResources) foreach (ProtoPartResourceSnapshot res in P.resources)
+                {
+                    float amount = 0;
+                    float.TryParse(res.resourceValues.GetValue("amount"), out amount);
+                    float unitCost = getResourcePrice(res.resourceName);
+                    if (unitCost != 0)
+                    {
+                        Resources.Add(res.resourceName, amount);
+                        Funding.Instance.Funds -= unitCost * amount;
+                    }
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Recursively count all the parts in P
+        /// </summary>
+        /// <param name="dic">total is stored in this dictionnay</param>
+        /// <param name="P">current part being scanned</param>
+        public static Dictionary<AvailablePart, int> ListAllParts(Part P, Dictionary<AvailablePart, int> dic = null)
+        {
+            if (dic == null) dic = new Dictionary<AvailablePart, int>();
+            AvailablePart id = P.partInfo;
+            int qty = 1 + P.symmetryCounterparts.Count;
+            if (dic.ContainsKey(id)) dic[id] += qty;
+            else dic[id] = qty;
+
+
+            PartResource r;
+            foreach (Part C in P.children) ListAllParts(C, dic);
+            return dic;
+        }
+        /// <summary>
+        /// Count all the parts in a list of parts. This process is not recursive.
+        /// </summary>
+        /// <param name="list">list of parts to count</param>
+        /// <returns>A dictionnary containing the Available parts and their quantity</returns>
+        public static Dictionary<AvailablePart, int> ListAllParts(List<Part> list)
+        {
+            Dictionary<AvailablePart, int> result = new Dictionary<AvailablePart, int>();
+
+            foreach (Part P in list)
+            {
+                AvailablePart id = P.partInfo;
+                if (result.ContainsKey(id)) result[id] += 1;
+                else result[id] = 1;
+            }
+            return result;
+        }
+        public static Dictionary<string, double> ListAllResources(List<Part> list)
+        {
+            Dictionary<string, double> result = new Dictionary<string, double>();
+            foreach (Part P in list)
+            {
+                foreach (PartResource R in P.Resources)
+                {
+                    string rname = R.resourceName;
+                    if (result.ContainsKey(rname)) result[rname] += R.amount;
+                    else result[rname] = R.amount;
+                    Debug.Log(P.name + "." + R.resourceName + " = " + R.amount);
+                }
+            }
+            return result;
+        }
+        public static Boolean isExperimental(AvailablePart myPart)
+        {
+            return ResearchAndDevelopment.GetTechnologyState(myPart.TechRequired) != RDTech.State.Available;
+        }
+        public static Boolean hasResourceCode(string name)
+        {
+            return resourceCodes.ContainsKey(name);
+        }
+        public static Boolean isResource(string name)
+        {
+            return PartResourceLibrary.Instance != null && PartResourceLibrary.Instance.resourceDefinitions[name] != null;
+        }
+        public static string getResourceCode(string name)
+        {
+            if (String.IsNullOrEmpty(name))
+            {
+                string code = String.Empty;
+                if (resourceCodes.TryGetValue(name, out code)) return code;
+                else return name.Length > 2? name.Substring(0, 2): name;
+            }
+            else return "Xx";
+        }
+        public static float getResourcePrice(string R)
+        {
+            return isResource(R)? PartResourceLibrary.Instance.resourceDefinitions[R].unitCost:0;
+        }
+        public static float getPartRawPrice(AvailablePart P)
+        {
+            float price = P.cost;
+            foreach (AvailablePart.ResourceInfo ri in P.resourceInfos)
+            {
+                int idx = ri.info.LastIndexOf("Cost:");
+                if (idx != -1)
+                {
+                    string coststr = ri.info.Substring(idx + 5);
+                    float c = 0;
+                    if (float.TryParse(coststr, out c))
+                    {
+                        price -= c;
+                    }
+                }
+            }
+            return price;
+        }
+        #endregion
+    }
+
+    /// <summary>
+    /// Runs only once, registers all game events.
+    /// </summary>
+    [KSPAddon(KSPAddon.Startup.EveryScene, true)]
+    class Launcher : MonoBehaviour
+    {
+
+        public void Start()
+        {
+            // Persistence
+            GameEvents.onGameStateLoad.Add(onGameStateLoad);
+            GameEvents.onGameStateSave.Add(onGameStateSave);
+            // Recovery/Construction
+            GameEvents.onVesselRecovered.Add(OnVesselRecovered);
+            GameEvents.OnVesselRollout.Add(OnVesselRollout);
+        }
+
+        #region I/O
+        public void onGameStateLoad(ConfigNode root)
+        {
+            Scrapyard.Instance.Rollback(root);
+        }
+        public void onGameStateSave(ConfigNode root)
+        {
+            Scrapyard.Instance.Commit(root);
+        }
+
+        void OnVesselRollout(ShipConstruct construct)
+        {
+            Debug.Log("Rolling out vessel : Removing parts from inventory");
+            Scrapyard.Instance.RolloutVessel(construct);
+        }
+
+        void OnVesselRecovered(ProtoVessel proto)
+        {
+            Debug.Log("Recovering ship " + proto.vesselName + ": Adding parts to Inventory");
+            Scrapyard.Instance.RecoverVessel(proto);
+        }
+        #endregion
+
+
+    }
+}
